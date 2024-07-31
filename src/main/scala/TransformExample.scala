d94c5e88fabd58102e9522c019af3756f33d1ff5
Ilya Kleyn 2000 <ilyakleyn2000new@gmail.com>
2024-07-31 22:04:06 +0300

    transform func study

---
+++ src/main/scala/TransformExample.scala
@@ -0,0 +1,32 @@
+package com.example
+
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
+
+object TransformExample extends App {
+
+  val custSchema = StructType(
+    Array(
+      StructField("ap_id", IntegerType, true),
+      StructField("ap_state", StringType, true),
+      StructField("ap_salary", IntegerType, true)
+    )
+
+  )
+
+  val spark = SparkSession.builder()
+    .appName("transform_example")
+    .master("local[*]")
+    .getOrCreate()
+
+  val apPath = "src/main/resources/ap_data.csv"
+
+  val DF=spark.read.option("delimiter", ",").option("header", "true").csv(apPath)
+
+  def doubleSal(df: DataFrame) : DataFrame ={
+      df.withColumn("double_salary", df("salary")*2)
+  }
+
+  val DF2=DF.transform(doubleSal)
+  DF2.show()
+}
